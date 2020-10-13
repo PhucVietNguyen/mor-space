2cc9768dd760e405c449655a1469ab9f43aca423
datpt007 <tuandat7792#gmail.com>
2020-10-13 11:10:03 +0700

    commit code 1

---
+++ service-registry/src/main/java/com/mor/space/RegistryApp.java
@@ -0,0 +1,13 @@
+package com.mor.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApp.class, args);
+    }
+}
